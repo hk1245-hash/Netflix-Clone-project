de1d87a91f8d934670e78749f5025839334630db
hk1245-hash <hk8208676@gmail.com>
2025-09-28 20:04:34 +0530

    5commit

---
+++ net.js
@@ -0,0 +1,42 @@
+
+// console.log(idf)
+
+
+// functionscrollleftbtn()
+// {
+//         const container=document.querySelector(".card-container")
+//         container.scrollBy({
+//             left:-300,
+//             behavior:"smooth"
+//         })
+//     }
+//         functionscrollrightbtn()
+//         {
+//        const container=document.querySelector(".card-container")
+//         container.scrollBy({
+//             left:300,
+//             behavior:"smooth"
+//         })  
+        
+    
+//     }
+
+
+//       const questions=document.querySelectorAll(".faq-question");
+//     question.forEach(btn=>{btn.addEventListener("click",()=>{
+//         const answer=btn.nextElementSibling;
+
+//         if(answer.style.display==="block"){
+//             answer.style.display="none";
+
+//         }
+//         else{
+//             answer.style.display="block";
+//         }
+
+//         questions.forEach(otherbtn=>{
+//             if(otherbtn!=btn){
+//                 otherbtn.nextElementSibling.style.display="none";
+//             }
+//         })
+//     })})
